dfb6f4d3c6bc650b17c49a7f5e965f4e71ac3d84
ashmikan <ashmika.nathali123@gmail.com>
2026-02-14 11:49:42 +0530

    Implement basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
   return (
     <div className="App">
+      <div className="container mt-4">
+
+        <h1 className="text-center mb-4">
+          Online Resume Builder
+        </h1>
+
+        <div className="row">
+
+          <div className="col-md-6">
+            ResumeForm
+              
+            
+          </div>
+
+          <div className="col-md-6">
+            ResumePreview  
+
+            <button
+              className="btn btn-primary mt-3"
       
+            >
+              Download PDF
+            </button>
+
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
