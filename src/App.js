d8be257376bff47ad5ea617d7bd62948326b72b9
ashmikan <ashmika.nathali123@gmail.com>
2026-02-14 19:20:10 +0530

    Implement PDF download functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React, { useState } from "react";
 import ResumeForm from "./components/ResumeForm";
 import ResumePreview from "./components/ResumePreview";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 function App() {
   const [formData, setFormData] = useState({
@@ -13,6 +15,23 @@ function App() {
     experience: "",
     photo: ""
   });
+
+  const downloadPDF = () => {
+
+    const input = document.getElementById("resume");
+
+    html2canvas(input).then((canvas) => {
+
+      const imgData = canvas.toDataURL("image/png");
+
+      const pdf = new jsPDF();
+
+      pdf.addImage(imgData, "PNG", 10, 10);
+
+      pdf.save("resume.pdf");
+
+    });
+  };
 
 
   return (
@@ -37,6 +56,7 @@ function App() {
 
             <button
               className="btn btn-primary mt-3"
+              onClick={downloadPDF}
       
             >
               Download PDF
