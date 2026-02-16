207e2bcc74ecb067953833ebc1430cc53e358aa2
ashmikan <ashmika.nathali123@gmail.com>
2026-02-16 18:19:12 +0530

    Add header section to ResumePreview

--- src/components/ResumePreview.js
+++ src/components/ResumePreview.js
@@ -11,21 +11,32 @@ function ResumePreview({ formData }) {
         width: "600px"
       }}
     >
+      {/* Header Section: Image + Name/Position in 2 columns */}
+      <div style={{ display: "flex", gap: "20px", alignItems: "center", marginBottom: "20px" }}>
+        {/* Column 1: Image */}
+        <div>
+          {formData.photo && (
+            <img
+              src={formData.photo}
+              alt="profile"
+              style={{
+                width: "120px",
+                height: "120px",
+                borderRadius: "50%",
+                objectFit: "cover"
+              }}
+            />
+          )}
+        </div>
 
-      {formData.photo && (
-        <img
-          src={formData.photo}
-          alt="profile"
-          style={{
-            width: "120px",
-            height: "120px",
-            borderRadius: "50%"
-          }}
-        />
-      )}
+        {/* Column 2: Name & Position */}
+        <div>
+          <h3 style={{ margin: "0 0 5px 0" }}>{formData.name}</h3>
+          <h5 style={{ margin: "0" }}>{formData.position}</h5>
+        </div>
+      </div>
 
-      <h3>{formData.name}</h3>
-        <h5>{formData.position}</h5>
+      <hr style={{ border: "none", borderTop: "3px solid #013880ff", margin: "20px 0" }}></hr>
       <p>Email: {formData.email}</p>
       <p>Phone: {formData.phone}</p>
 
